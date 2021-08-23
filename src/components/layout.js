75abc155c0c91d90d15e0219f7f0173cbcddb03a
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-08-23 09:10:15 -0400

    moved bio below nav in post

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,7 +23,7 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
-      <main>{children}</main>
+      <main className='mainClass'>{children}</main>
       <footer className='siteFooter'>
         <div className='container'>
         Copyright © {new Date().getFullYear()}, Built by
